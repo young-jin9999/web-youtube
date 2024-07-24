30ea36f3d9a640a0baf233e99396fe58917cf5c7
young-jin9999 <skdlxld012@gmail.com>
2024-07-24 16:07:03 +0900

    SCSS 세팅

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './assets/scss/style.scss'
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
